d0e0494a71103855e0eee1e9a52c4e54791840a9
Afonso <afonso.noliveira29@gmail.com>
2020-09-20 20:16:25 -0300

    Create MovieRow component

--- src/components/MovieRow/styles.js
+++ src/components/MovieRow/styles.js
@@ -3,20 +3,56 @@ import styled from "styled-components";
 export const MovieRow = styled.div`
   margin-bottom: 30px;
 
+  & .left,
+  .right {
+    position: absolute;
+    width: 42px;
+    height: 225px;
+    background-color: rgba(0, 0, 0, 0.6);
+    z-index: 99;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    opacity: 0;
+    transition: all ease 0.5;
+  }
+
+  & .left {
+    left: 0;
+  }
+
+  & .right {
+    right: 0;
+  }
+
+  &:hover .left,
+  :hover .right {
+    opacity: 1;
+  }
+
   & h2 {
-    margin: 0px 0px 0px 30px;
+    margin: 0px 0px 8px 42.75px;
+    font-size: 1.5em;
   }
   & .movieRow-list-area {
     overflow-x: hidden;
-    padding-left: 30px;
+    padding-left: 42.75px;
   }
+
+  & .movieRow-list {
+    transition: all ease 0.5;
+  }
+
   & .movieRow-cover {
     display: inline-block;
-    width: 150px;
     cursor: pointer;
+    width: 400px;
     & img {
+      border-radius: 5px;
       width: 100%;
-      transform: scale(0.9);
+      height: 225px;
+      transform: scale(0.98);
       transition: all ease 0.2s;
       &:hover {
         transform: scale(1);
